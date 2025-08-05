d3aca9383646d8cd7aed2d4fefb413a4a978407b
Xullim <julioc4@ufu.br>
2025-08-05 16:37:19 -0300

    :sparkles: feat: Início do Menu e ajuste de configurações

--- src/main/java/br/com/alura/ChallengeLiterAlura/ChallengeLiterAluraApplication.java
+++ src/main/java/br/com/alura/ChallengeLiterAlura/ChallengeLiterAluraApplication.java
@@ -1,13 +1,28 @@
 package br.com.alura.ChallengeLiterAlura;
 
+import br.com.alura.ChallengeLiterAlura.Principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
-public class ChallengeLiterAluraApplication {
+public class ChallengeLiterAluraApplication implements CommandLineRunner {
+
+	private final ConfigurableApplicationContext context;
+
+	public ChallengeLiterAluraApplication(ConfigurableApplicationContext context) {
+		this.context = context;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeLiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args){
+		Principal principal = new Principal(context);
+		principal.exibeMenu();
+	}
 }
---
+++ src/main/java/br/com/alura/ChallengeLiterAlura/Principal/Principal.java
@@ -0,0 +1,50 @@
+package br.com.alura.ChallengeLiterAlura.Principal;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Scanner;
+
+@AllArgsConstructor
+public class Principal {
+
+    private final ConfigurableApplicationContext context;
+
+    //https://gutendex.com/books/?search=Dom%20Casmurro
+
+    public void exibeMenu() {
+
+        Scanner sc = new Scanner(System.in);
+        int escolha = -1;
+
+        while (escolha != 0) {
+            System.out.println("Escolha o número de sua opção: ");
+            System.out.println("1 - Buscar livro pelo título");
+            System.out.println("2 - Listar livros registrados");
+            System.out.println("3 - Listar autores registrados");
+            System.out.println("4 - Listar autores vivos em um determinado ano");
+            System.out.println("5 - Listar livros em um determinado idioma");
+            System.out.println("0 - Sair");
+
+            escolha = sc.nextInt();
+            sc.nextLine();
+
+            switch (escolha) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+        }
+
+        System.out.println("Encerrando a aplicação!");
+        context.close();
+    }
+}
